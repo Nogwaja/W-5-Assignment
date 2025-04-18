ea327a4787f940591de445453142294f68a4a4c1
nogwaja <ave.tyeba@gmail.com>
2025-04-18 12:23:04 +0200

    Create test_vehicle.py

---
+++ test_vehicle.py
@@ -0,0 +1,11 @@
+# Test the function with different vehicle types
+car = Car()
+plane = Plane()
+boat = Boat()
+bike = Bicycle()
+
+# Demonstrating polymorphism
+perform_movement(car)
+perform_movement(plane)
+perform_movement(boat)
+perform_movement(bike)
